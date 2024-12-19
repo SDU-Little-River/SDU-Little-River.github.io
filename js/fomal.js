eb15cd6d0d1101260043ef22c5816a31b6c892e6
xhbsh <qq260342855@gmail.com>
2024-12-19 22:46:37 +0800

    Site updated: 2024-12-19 22:46:37

--- js/fomal.js
+++ js/fomal.js
@@ -79,7 +79,7 @@ function createtime() {
   var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
   var unit = (dis / 149600000).toFixed(6);  // 天文单位
   // 网站诞生时间
-  var grt = new Date("08/09/2022 00:00:00");
+  var grt = new Date("12/17/2024 00:00:00");
   var days = (now - grt) / 1e3 / 60 / 60 / 24,
       dnum = Math.floor(days),
       hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
@@ -91,11 +91,8 @@ function createtime() {
   var seconds = (now - grt) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum,
       snum = Math.round(seconds);
   1 == String(snum).length && (snum = "0" + snum);
-  let currentTimeHtml = "";
-  (currentTimeHtml =
-      hnum < 18 && hnum >= 9
-          ? `<img class='boardsign' src='https://lskypro.acozycotage.net/Fomalhaut/badge/F小屋-科研摸鱼中.svg' title=''><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> </div>`
-          : `<img class='boardsign' src='https://lskypro.acozycotage.net/Fomalhaut/badge/F小屋-下班休息啦.svg' title=''><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> </div>`),
+  let currentTimeHtml = `<div style="font-size:13px;font-weight:bold">本站运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i></div>`;
+  // 将时间显示在页面上
   document.getElementById("workboard") &&
   (document.getElementById("workboard").innerHTML = currentTimeHtml);
 }
