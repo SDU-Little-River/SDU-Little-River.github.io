f65cbd3edf7fc9a7e912570706a54b00aa8847cd
xhbsh <qq260342855@gmail.com>
2024-12-19 22:38:09 +0800

    Site updated: 2024-12-19 22:38:08

--- js/fomal.js
+++ js/fomal.js
@@ -68,3 +68,40 @@ document.onkeydown = function (e) {
   }
 };
 /* 禁用f12与按键防抖 end */
+
+
+/* 页脚计时器 start */
+var now = new Date();
+function createtime() {
+  // 当前时间
+  now.setTime(now.getTime() + 1000);
+  var start = new Date("08/01/2022 00:00:00"); // 旅行者1号开始计算的时间
+  var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
+  var unit = (dis / 149600000).toFixed(6);  // 天文单位
+  // 网站诞生时间
+  var grt = new Date("08/09/2022 00:00:00");
+  var days = (now - grt) / 1e3 / 60 / 60 / 24,
+      dnum = Math.floor(days),
+      hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
+      hnum = Math.floor(hours);
+  1 == String(hnum).length && (hnum = "0" + hnum);
+  var minutes = (now - grt) / 1e3 / 60 - 1440 * dnum - 60 * hnum,
+      mnum = Math.floor(minutes);
+  1 == String(mnum).length && (mnum = "0" + mnum);
+  var seconds = (now - grt) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum,
+      snum = Math.round(seconds);
+  1 == String(snum).length && (snum = "0" + snum);
+  let currentTimeHtml = "";
+  (currentTimeHtml =
+      hnum < 18 && hnum >= 9
+          ? `<img class='boardsign' src='https://lskypro.acozycotage.net/Fomalhaut/badge/F小屋-科研摸鱼中.svg' title=''><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> </div>`
+          : `<img class='boardsign' src='https://lskypro.acozycotage.net/Fomalhaut/badge/F小屋-下班休息啦.svg' title=''><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> </div>`),
+  document.getElementById("workboard") &&
+  (document.getElementById("workboard").innerHTML = currentTimeHtml);
+}
+// 设置重复执行函数，周期1000ms
+setInterval(() => {
+  createtime();
+}, 1000);
+
+/*页脚计时器 end */
